f59a0a59251f1832c582641916fd8b1ffde599db
michalwegiel <44325515+michalwegiel@users.noreply.github.com>
2023-04-02 14:54:04 +0200

    Delete build/lib/python_server directory

--- build/lib/python_server/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa: F401
-
-from http_response import HTTPResponse
-from request_handler import handle_request
-from response_factory import response_factory_get
-from server import TCPServer
